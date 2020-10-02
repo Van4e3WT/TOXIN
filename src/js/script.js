bf6c7140062214c2b7c5448a3dcae91ce2b08b2c
IvanG <van4e3wt@yandex.ru>
2020-10-02 13:34:35 +0500

    Customized datepicker and structed SCSS files

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,25 @@
+//############# header-burger #############
 function removeActive() {
-    $('.header__burger, .header__section').removeClass('active');
+    $(".header__burger, .header__section").removeClass("active");
 }
 
-$('.header__burger').on('click', function(e) {
+$(".header__burger").on("click", function(e) {
     e.PreventDefault;
-    $('.header__burger, .header__section').toggleClass('active');
+    $(".header__burger, .header__section").toggleClass("active");
 });
 
-$('.header__section a').on('click', removeActive());
+$(".header__section a").on("click", removeActive());
 
-$(window).on('resize',function (){
+$(window).on("resize",function (){
     if ($(this).width() > 1250) {
         removeActive();
     }
+});
+
+//############# calendar-datepicker #############
+
+$(".datepicker-here").datepicker({
+    minDate: new Date(),
+    clearButton: true,
+    todayButton: true,
 });
