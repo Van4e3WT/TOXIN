a50db2df7437c186d723801e93a5c8bf1fc9043b
IvanG <van4e3wt@yandex.ru>
2020-10-08 12:21:49 +0500

    Finished HTML codding landing page

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,12 @@ dateDeparture.onclick = () => $("#dateArrival").datepicker().data('datepicker').
 //############# landing__guests #############
 
 guests.onclick = () => $(".dropdown-list").toggleClass("active");
+
+//функция словоформы для массива из трёх возможных вариантов (один/одна, два/две, много)
+function num2str(n, text_forms) {  
+    n = Math.abs(n) % 100; var n1 = n % 10;
+    if (n > 10 && n < 20) { return text_forms[2]; }
+    if (n1 > 1 && n1 < 5) { return text_forms[1]; }
+    if (n1 == 1) { return text_forms[0]; }
+    return text_forms[2];
+  }
