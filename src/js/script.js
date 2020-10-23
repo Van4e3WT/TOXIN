b76739b33c6fff3b8efc5835a29d12a953d63e10
IvanG <van4e3wt@yandex.ru>
2020-10-23 14:00:50 +0500

    Created mixins and changed datepicker logic

--- src/js/script.js
+++ src/js/script.js
@@ -4,24 +4,31 @@ $(".header__burger").on("click", () => $(".header__burger, .header__section").to
 
 $(".header__section a").on("click", () => $(".header__burger, .header__section").removeClass("active"));
 
-//############# landing__calendar-datepicker #############
+//############# twin-datepick #############
 
 $("#dateArrival").datepicker({
     clearButton: true,
     todayButton: true,
     minDate: new Date(),
 });
-dateDeparture.onclick = () => $("#dateArrival").datepicker().data('datepicker').show();
 
-//############# landing__guests #############
+$("#dateDeparture").on("click", () => $("#dateArrival").datepicker().data('datepicker').show());
 
-guests.onclick = () => $(".dropdown-list").toggleClass("active");
+//############# date-solo #############
 
-//функция словоформы для массива из трёх возможных вариантов (один/одна, два/две, много)
-function num2str(n, text_forms) {
-    n = Math.abs(n) % 100; var n1 = n % 10;
-    if (n > 10 && n < 20) { return text_forms[2]; }
-    if (n1 > 1 && n1 < 5) { return text_forms[1]; }
-    if (n1 == 1) { return text_forms[0]; }
-    return text_forms[2];
-}
+$("#dateSolo").datepicker({
+    clearButton: true,
+    todayButton: true,
+    minDate: new Date(),
+});
+
+//############# date-solo-range #############
+
+$("#dateSoloRange").datepicker({
+    clearButton: true,
+    todayButton: true,
+    minDate: new Date(),
+});
+
+//############# dropdown-guests #############
+$("#guestsDropdown").on("click", () => $(".guests-dropdown__list").toggleClass("active"));
