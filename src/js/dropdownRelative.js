77cc6c3250929182bbe663bcd51e492102f2a4c5
IvanG <van4e3wt@yandex.ru>
2021-01-19 15:01:29 +0500

    Ready for github pages

--- src/js/dropdownRelative.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let drops = document.querySelectorAll('.dropdown-checkbox');
-
-  drops.forEach(item => {
-    item.addEventListener('click', () => {
-      item.querySelector('.dropdown-checkbox__title').classList.toggle('active');
-    });
-  });
-});
