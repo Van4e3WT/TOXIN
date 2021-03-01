34f3374d7e1ed5522e362e823884aecc3867da71
IvanG <van4e3wt@yandex.ru>
2021-03-01 16:05:23 +0500

    Included plugin multislider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // JS
 import "./js/datepicker.js"
 import "./js/script.js"
-import "./js/sliderRange.js"
 import "./js/dropdownAbsolute.js"
 import "./js/dropdownRelative.js"
 import "./js/carousel.js"
+import "./js/multislider-v43.js"
 
 // SCSS
 import "./scss/style.scss"
 
 //CSS
 import "./css/datepicker.css"
+import "./css/multislider-v43.css"
