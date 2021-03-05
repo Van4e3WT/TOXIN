5c6b7a31c69c57bcb462b242f46bf68fbfd31c27
IvanG <van4e3wt@yandex.ru>
2021-03-05 14:19:23 +0500

    Updated header and contained mixins

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,40 @@
 //############# header-burger #############
-$('.header__burger').on('click', () => {
-  $('.header__burger, .header__section').toggleClass('active');
-});
-
-$('.header__section a').on('click', () => {
-  $('.header__burger, .header__section').removeClass('active');
+$('.header-content__burger').on('click', () => {
+  $('.header-content__burger, .header-content__body').toggleClass('active');
 });
 
 //############# twin-datepick #############
 let twinDatepickers = $('.twin-datepick');
 
-$.each(twinDatepickers, function(index, value) {
-    let dateArrival = $(value).find('.dateArrival');
-    let dateDeparture = $(value).find('.dateDeparture');
+$.each(twinDatepickers, function (index, value) {
+  let dateArrival = $(value).find('.dateArrival');
+  let dateDeparture = $(value).find('.dateDeparture');
 
-    dateArrival.datepicker({
-        clearButton: true,
-        todayButton: true,
-        minDate: new Date(),
-    });
+  dateArrival.datepicker({
+    clearButton: true,
+    todayButton: true,
+    minDate: new Date(),
+  });
 
-    $(dateDeparture).on('click', () => dateArrival.datepicker().data('datepicker').show());
+  $(dateDeparture).on('click', () => dateArrival.datepicker().data('datepicker').show());
 });
 
 //############# date-solo #############
 $('.dateSolo').datepicker({
-    clearButton: true,
-    todayButton: true,
+  clearButton: true,
+  todayButton: true,
 });
 
 //############# date-solo-range #############
 $('.dateSoloRange').datepicker({
-    clearButton: true,
-    todayButton: true,
-    minDate: new Date(),
+  clearButton: true,
+  todayButton: true,
+  minDate: new Date(),
 });
 
 //############# opened-datepick #############
 $('.opened-datepick').datepicker({
-    inline: true,
-    clearButton: true,
-    todayButton: true,
+  inline: true,
+  clearButton: true,
+  todayButton: true,
 });
