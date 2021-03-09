4d9742ff51e27c042424aa31b70f87325a2d33a0
IvanG <van4e3wt@yandex.ru>
2021-03-09 12:07:02 +0500

    Updated datepicker css to default and forked

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import "./js/dropdownRelative.js"
 import "./js/carousel.js"
 import "./js/multislider-v43.js"
 
-// SCSS
-import "./scss/style.scss"
-
 //CSS
 import "./css/datepicker.css"
 import "./css/multislider-v43.css"
+
+// SCSS
+import "./scss/style.scss"
