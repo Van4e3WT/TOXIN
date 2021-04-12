6adc33200d1b01ece2f19462879c3b721a8ce329
IvanG <van4e3wt@yandex.ru>
2021-04-12 12:51:38 +0500

    Updated file struct pluggable project's modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // JS
-import './js/multislider-v43';
-import './js/datepickerFork';
+import './libs/multislider-v43/multislider-v43';
+import './libs/air-datepicker/datepicker-fork';
 import './blocks/blocks-scripts';
 
 // CSS
 import 'air-datepicker/dist/css/datepicker.min.css';
-import './css/multislider-v43.css';
+import './libs/multislider-v43/multislider-v43.css';
 
 // SCSS
 import './scss/style.scss';
