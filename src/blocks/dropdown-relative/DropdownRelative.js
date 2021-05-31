08408480c51f1cca2b4ae0b2cb6f5beebe10cf88
IvanG <van4e3wt@yandex.ru>
2021-05-31 12:12:15 +0500

    fix issue #19 dropdown-relative

--- src/blocks/dropdown-relative/DropdownRelative.js
+++ src/blocks/dropdown-relative/DropdownRelative.js
@@ -5,15 +5,16 @@ class DropdownRelative {
   }
 
   init() {
-    this.dropdown.addEventListener('click', this.toggleActiveDropdown.bind(this));
+    this.title = this.dropdown.querySelector(`.${this.selector}__content`);
+
+    this.title.addEventListener('click', this.toggleActiveDropdown.bind(this));
   }
 
   toggleActiveDropdown() {
-    const title = this.dropdown.querySelector(`.${this.selector}__content`);
     const list = this.dropdown.querySelector(`.${this.selector}__list`);
 
     this.dropdown.classList.toggle(`${this.selector}_active`);
-    title.classList.toggle(`${this.selector}__content_active`);
+    this.title.classList.toggle(`${this.selector}__content_active`);
     list.classList.toggle(`${this.selector}__list_active`);
   }
 }
