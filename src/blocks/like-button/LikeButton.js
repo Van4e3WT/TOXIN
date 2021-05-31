cc18b6429bfb304cc280a834784817200933aafe
IvanG <van4e3wt@yandex.ru>
2021-05-31 13:17:44 +0500

    fix issue #19 like-button

---
+++ src/blocks/like-button/LikeButton.js
@@ -0,0 +1,24 @@
+class LikeButton {
+  constructor(like, selector) {
+    this.like = like;
+    this.selector = selector;
+  }
+
+  init() {
+    this.input = this.like.querySelector(`.${this.selector}__input`);
+
+    this.input.addEventListener('change', this.moveCount.bind(this));
+  }
+
+  moveCount() {
+    const counter = this.like.querySelector(`.${this.selector}__counter`);
+
+    if (this.input.checked) {
+      counter.textContent = parseInt(counter.textContent, 10) + 1;
+    } else {
+      counter.textContent = parseInt(counter.textContent, 10) - 1;
+    }
+  }
+}
+
+export default LikeButton;
--- src/blocks/like-button/like-button.js
+++ src/blocks/like-button/like-button.js
@@ -1,1 +1,18 @@
+/* global document */
+
 import './like-button.scss';
+import LikeButton from './LikeButton';
+
+const BLOCKNAME = 'like-button';
+
+function initLikesCount() {
+  const likes = document.querySelectorAll(`.js-${BLOCKNAME}`);
+
+  likes.forEach((item) => {
+    const like = new LikeButton(item, BLOCKNAME);
+
+    like.init();
+  });
+}
+
+document.addEventListener('DOMContentLoaded', initLikesCount);
