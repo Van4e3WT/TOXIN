a0f0e8e0291a23fccbd86fa453e168983650beaa
IvanG <ivan.gladyshev@mail.ru>
2021-09-10 10:32:34 +0500

    Fix issue #28 remove useless file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-import './style';
+import './libs/air-datepicker/datepicker-fork';
+import './libs/multislider-v43/multislider-v43-fork';
+import './style.scss';
--- src/style.js
+++
@@ -1,3 +0,0 @@
-import './libs/air-datepicker/datepicker-fork';
-import './libs/multislider-v43/multislider-v43-fork';
-import './style.scss';
